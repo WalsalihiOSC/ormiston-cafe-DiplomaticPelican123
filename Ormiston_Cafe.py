8825ab91399933432faa6dcf8a7ec62bfe3d69d2
DiplomaticPelican123 <st20123@ormiston.school.nz>
2022-06-03 09:58:42 +1200

    Added new class for menu (touch ups)

--- Ormiston_Cafe.py
+++ Ormiston_Cafe.py
@@ -5,16 +5,23 @@ class Main:
 
     self.root = root
 
-    intro_frame = Frame(root, width = 250, height = 250, highlightbackground = "black", highlightthickness = 1)
-    intro_frame.place(x = 250, y = 100)
+    self.intro_frame = Frame(root, width = 250, height = 250, highlightbackground = "black", highlightthickness = 1)
+    self.intro_frame.place(x = 250, y = 100)
 
-    intro_label = Label(intro_frame, text = "Ormiston Senior College Maca's", anchor = CENTER)
+    intro_label = Label(self.intro_frame, text = "Ormiston Senior College Maca's", anchor = CENTER)
     intro_label.place(x = 35, y = 110)
 
-    osc_logo = PhotoImage("Image/osc_logo.jpg")
+    start_button = Button(self.intro_frame, text = "Order Now", command = self.menu)
+    start_button.place(x = 179, y = 222)
 
-    img_label = Label(root, image = osc_logo, width=250, height=250)
-    img_label.grid()
+  def menu(self):
+    self.intro_frame.destroy()
+    Menu(root)
+
+
+class Menu:
+  def __init__(self, root):
+    self.root = root
     
 
 
